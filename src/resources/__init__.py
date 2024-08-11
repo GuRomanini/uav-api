22a50f4cd4ed8b34311fe6b0ff3aec243d789a93
Gustavo Romanini <romaninigtv@gmail.com>
2024-08-11 20:33:33 -0300

    feat(resource init file): add imports

---
+++ src/resources/__init__.py
@@ -0,0 +1,7 @@
+from resources.service_resource import ServiceResource
+
+
+from resources.health_check import HealthcheckResource
+from resources.home import Home
+from resources.sink import SinkResource
+from resources.time import TimeResource
