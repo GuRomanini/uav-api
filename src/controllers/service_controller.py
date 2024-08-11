a22045208ac6439770c65824d7bad0226f12c36d
Gustavo Romanini <romaninigtv@gmail.com>
2024-08-11 20:34:07 -0300

    feat(controllers): add service controller

---
+++ src/controllers/service_controller.py
@@ -0,0 +1,18 @@
+from connectors import ServiceHandlerConnector
+from mappers import ServiceMapper
+from utils.context import Context
+from utils.logger import Logger
+
+
+class ServiceController:
+    def __init__(self, context: Context) -> None:
+        self.context: Context = context
+        self.logger = Logger(context, __class__)
+
+    def register_service(self, service_data: dict) -> dict:
+        self.logger.debug(f"Registering Service: {service_data['service_name']}")
+
+        service_handler_connector = ServiceHandlerConnector(context=self.context)
+        return service_handler_connector.register_service(
+            service_name=service_data["service_name"], service_type=service_data["service_type"]
+        )
