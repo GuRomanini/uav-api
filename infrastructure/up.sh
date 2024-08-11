1ffabd0a7bedbe0a0ab17cb0dbd50954dbe95e40
Gustavo Romanini <romaninigtv@gmail.com>
2024-08-11 20:29:31 -0300

    feat(up script): stop running services

--- infrastructure/up.sh
+++ infrastructure/up.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Stop running services..."
+docker-compose -f ./infrastructure/docker-compose.yml stop
 echo "Removing existing services..."
-docker-compose rm
+docker-compose -f ./infrastructure/docker-compose.yml rm
 echo "Building app..."
-docker-compose build
+docker-compose -f ./infrastructure/docker-compose.yml build
 echo "Runing app.."
-docker-compose up
+docker-compose -f ./infrastructure/docker-compose.yml up
